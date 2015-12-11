ba677d97ecad6c24338dca21bf0fc9c4e8224a2c
Kayla Williams <kkayla.williams@icloud.com>
2015-12-11 14:49:11 -0500

    add additonal lightbox and images

--- kdot_lightbox_inclass/script.js
+++ kdot_lightbox_inclass/script.js
@@ -1,1 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function(){
+	$(".lightbox-link").on("mouseover",
+		// selecting the class light box link. listening speicifically for the click event on the anchor tag. the first thing we're passing in is click the second things is function. thenfinally selecting the outerlightbox class which is styled in css with a display of none an using jquery method show that sets it to be block, removes the diplay of none
+		function(e){
+		e.preventDefault();
+		// prevents the default action of opening a new page on when you click a link
+		console.log(this); 
+				
+
+		$(".outer-lightbox").show();
+		$(".outer-lightbox").on('click', function(){
+			$(".outer-lightbox").hide();
+
+		});
+	});
+});
+
